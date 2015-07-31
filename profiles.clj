113c1f9c97a1bcf1f8c972b3c89803fd5e383bc3
Hugo Duncan <hugo@hugoduncan.org>
2015-07-31 15:57:35 -0400

    Add dependencies to dev profile

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,9 @@
 {:dev {:source-paths ["src"],
-       :plugins [[lein-pallet-release "RELEASE"]]}
+       :plugins [[lein-pallet-release "RELEASE"]]
+       :dependencies [[org.clojure/clojure "1.6.0"]
+                      [prismatic/schema "0.4.3"]
+                      [cheshire "5.5.0"]
+                      [circleci/clj-yaml "0.5.3"]]}
  :test {:source-paths ["src"]
         :dependencies [[org.clojure/clojure "1.6.0"]
                        [prismatic/schema "0.4.3"]
