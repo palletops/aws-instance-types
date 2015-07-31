1614f4cad076a4d60b6f37f07f2b2f9496edbd7e
Hugo Duncan <hugo@hugoduncan.org>
2015-07-31 06:36:45 -0400

    Move dependencies to test profile

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,7 @@
 {:dev {:source-paths ["src"],
        :plugins [[lein-pallet-release "RELEASE"]]}
- :test {:source-paths ["src"]}}
+ :test {:source-paths ["src"]
+        :dependencies [[org.clojure/clojure "1.6.0"]
+                       [prismatic/schema "0.4.3"]
+                       [cheshire "5.5.0"]
+                       [circleci/clj-yaml "0.5.3"]]}}
--- project.clj
+++ project.clj
@@ -3,8 +3,4 @@
   :url "https://github.com/palletops/aws-instance-types"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ^:replace []
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [prismatic/schema "0.4.3"]
-                 [cheshire "5.5.0"]
-                 [circleci/clj-yaml "0.5.3"]])
+  :source-paths ^:replace [])
