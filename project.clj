c6e87de8b3a4dcc4fbb42252a379e3479a4aeb81
Hugo Duncan <hugo@hugoduncan.org>
2015-07-31 06:28:33 -0400

    Update project.clj for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/aws-instance-types "0.1.0"
+(defproject com.palletops/aws-instance-types "0.1.1-SNAPSHOT"
   :description "AWS Instance Type Data"
   :url "https://github.com/palletops/aws-instance-types"
   :license {:name "Eclipse Public License"
